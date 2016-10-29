810db62aaae8b84209f706266415997ad4401e2f
Imac <quienquieraquesea_123@hotmail.com>
2016-10-29 12:56:51 -0500

    se aplicó el número random

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-var jugador=prompt("¿Cuál es tu nombre?")
+var compu=Math.floor(Math.random() * 3)+1;
+console.log (compu)
+
+if (compu) {}
+
+// var juega=prompt("Piedra, papel o tijera");
+
+
+
+
+// {
+// var piedra=0;
+// var papel=1;
+// var tijera=2;
+// }
+
