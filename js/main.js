d645bd5fc7f0ecdb7f84b01cf5ad07b2fc4b51ca
Alberto Hernández <quienquieraquesea_123@hotmail.com>
2016-11-03 15:04:14 -0600

    ni idea

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var compu=Math.floor(Math.random() * 3)+1;
-console.log (compu)
+console.log (compu);
 
-if (compu) {}
+var jugador=parseInt(prompt("piedra, papel o tijera"));
+console.log (jugador);
+
+if (var jugador-"piedra";
+	
+papel=2;
+tijera=3;
+
+
+if (jugador >= 1) {}
 
 // var juega=prompt("Piedra, papel o tijera");
 
